a2bb7918feae42bc270172f55b9a4de1161c88fa
akashrajguru <akash.rajguru@est.tech>
2025-10-24 21:56:11 +0100

    Day one of go journey

---
+++ day1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	greeting := "Hello from Go!"
+	fmt.Println(greeting)
+	day := 1
+	fmt.Println("This is Day", day, "of my Go journey")
+	myGoal := "write 10 lines of code"
+	fmt.Println(myGoal)
+}
