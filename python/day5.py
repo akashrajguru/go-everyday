6dda211ea8018db45b8bf17abdcc57e9bec168c0
akashrajguru <a.rajguru@research.ait.ie>
2025-10-29 21:28:08 +0000

    Day 5 commit 2

---
+++ python/day5.py
@@ -0,0 +1,30 @@
+# PART 1 data types
+language = "Python"
+version = 3.11
+isLearning = True
+
+# Python has no zero values. Variable must be assigned before it can be used
+a_string = None
+an_int = 0
+a_bool = False
+
+print(language, version, isLearning)
+print("None value:", a_string)
+
+# PART 2: Operators
+
+x = 10
+y = 3
+
+print(f"Addition (x + y) = {x + y}")
+print(f"Subtraction (x - y) = {x -y}")
+print(f"Multiplication (x * y) = {x * y}")
+# There is difference in division
+print(f"Float Division (x /y) = {x / y}")
+print(f"Integer Division (x // y) = {x // y}")
+print(f"Modulus (x % y) = {x % y}")
+print(f"Exponent (x ** y) = {x ** y}")
+# 2. Comparison Operators (same as Go)
+print(f"x > y: {x > y}")
+print(f"x == 10: {x == 10}")
+print(f"y != 3: {y != 3}")
