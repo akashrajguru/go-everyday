81a41c42e8a8757463953da0aeafefb3ca28b8d8
akashrajguru <a.rajguru@research.ait.ie>
2026-01-03 23:27:15 +0000

    Added Interface for behavior conract

--- go-composition/composition-interface/main.go
+++ go-composition/composition-interface/main.go
@@ -1,7 +1,13 @@
 package main
 
 // Define interfaces for behavior contracts
+// Why it matters: By coding to interfaces rather than concrete types,
+// we can swap implementations easily - real database in production, fake one in tests.
 type Database interface {
 	Save(data string) error
 	Get(id string) (string, error)
+}
+
+type EmailSender interface {
+	Send(to, subject, body string) error
 }
